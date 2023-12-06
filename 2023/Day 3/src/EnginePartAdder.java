f69ca1799c09b7110b415a76ba2a76ed48cbb750
roleyguacamole <tyler.barry@uconn.edu>
2023-12-06 16:37:37 -0500

    Add solution in Java for Y2023 D03 Part 1

---
+++ 2023/Day 3/src/EnginePartAdder.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EnginePartAdder {
+    private static final Pattern numberPattern = Pattern.compile("\\d+");
+    private static final Integer MAX_LENGTH = 140;
+    private static final ArrayList<String> inputList = new ArrayList<String>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        final File input = new File("../data/input.txt");
+        final Scanner inputReader = new Scanner(input);
+
+        while (inputReader.hasNextLine()) {
+             inputList.add(inputReader.nextLine());
+        }
+        inputReader.close();
+
+        final Integer result = sumEngineParts();
+        System.out.println(String.format("Answer 1: %d", result));
+    }
+
+    private static Integer sumEngineParts() {
+        Integer rowSums = 0;
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            rowSums = rowSums + sumRowParts(inputList.get(i), i);
+        }
+        return rowSums;
+    }
+
+    private static Integer sumRowParts(String rowString, Integer rowNumber) {
+        Integer rowSum = 0;
+        final Matcher numberMatcher = numberPattern.matcher(rowString);
+        while (numberMatcher.find()) {
+            if (isEnginePart(numberMatcher.group(), numberMatcher.start(), rowNumber)) {
+                rowSum = rowSum + Integer.parseInt(numberMatcher.group());
+            }
+        }
+        return rowSum;
+    }
+
+    private static Boolean isEnginePart(String candidate, Integer col, Integer row) {
+        Boolean result = false;
+        for (int i = row - 1; i <= row +1; i++) {
+            for (int j = col - 1; j <= col + candidate.length(); j++) {
+                if (i >= 0 && i < MAX_LENGTH && j >= 0 && j < MAX_LENGTH) {
+                    if (!getCharAt(j, i).equals('.') && !Character.isDigit(getCharAt(j, i))) {
+                        result = true;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    
+    private static Character getCharAt(Integer col, Integer row) {
+        return inputList.get(row).charAt(col);
+    }
+}
