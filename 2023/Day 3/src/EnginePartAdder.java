df53809bd778080af6ebcbe067144e094d36088e
roleyguacamole <tyler.barry@uconn.edu>
2024-01-02 00:08:28 -0500

    Add solution in Java for Y2023 D03 Part 2

--- 2023/Day 3/src/EnginePartAdder.java
+++ 2023/Day 3/src/EnginePartAdder.java
@@ -1,14 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EnginePartAdder {
     private static final Pattern numberPattern = Pattern.compile("\\d+");
+    private static final Pattern gearPattern = Pattern.compile("\\*");
     private static final Integer MAX_LENGTH = 140;
     private static final ArrayList<String> inputList = new ArrayList<String>();
+    private static final HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> gearCandidates = new HashMap<>();
 
     public static void main(String[] args) throws FileNotFoundException {
         final File input = new File("../data/input.txt");
@@ -20,7 +23,9 @@ public class EnginePartAdder {
         inputReader.close();
 
         final Integer result = sumEngineParts();
+        final Integer resultTwo = sumGearRatios();
         System.out.println(String.format("Answer 1: %d", result));
+        System.out.println(String.format("Answer 2: %d", resultTwo));
     }
 
     private static Integer sumEngineParts() {
@@ -31,12 +36,32 @@ public class EnginePartAdder {
         return rowSums;
     }
 
+    private static Integer sumGearRatios() {
+        createGearCandidatesMap();
+        Integer rowSums = 0;
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            rowSums = rowSums + sumRowGearRatios(inputList.get(i), i);
+        }
+        return rowSums;
+    }
+
     private static Integer sumRowParts(String rowString, Integer rowNumber) {
         Integer rowSum = 0;
         final Matcher numberMatcher = numberPattern.matcher(rowString);
         while (numberMatcher.find()) {
             if (isEnginePart(numberMatcher.group(), numberMatcher.start(), rowNumber)) {
                 rowSum = rowSum + Integer.parseInt(numberMatcher.group());
+            }
+        }
+        return rowSum;
+    }
+
+    private static Integer sumRowGearRatios(String rowString, Integer rowNumber) {
+        Integer rowSum = 0;
+        final Matcher gearMatcher = gearPattern.matcher(rowString);
+        while (gearMatcher.find()) {
+            if (isGear(gearMatcher.start(), rowNumber)) {
+                rowSum = rowSum + getGearRatio(gearMatcher.start(), rowNumber);
             }
         }
         return rowSum;
@@ -54,6 +79,44 @@ public class EnginePartAdder {
             }
         }
         return result;
+    }
+
+    private static Boolean isGear(Integer col, Integer row) {
+        return gearCandidates.get(row).get(col).size() == 2;
+    }
+
+    private static Integer getGearRatio(Integer col, Integer row) {
+        ArrayList<Integer> gears = gearCandidates.get(row).get(col);
+        return gears.get(0) * gears.get(1);
+    }
+
+    private static void createGearCandidatesMap() {
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            final HashMap<Integer, ArrayList<Integer>> columnToInts = new HashMap<>();
+            for (int j = 0; j < MAX_LENGTH; j++) {
+                columnToInts.put(j, new ArrayList<>());
+            }
+            gearCandidates.put(i, columnToInts);
+        }
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            final String rowString = inputList.get(i);
+            final Matcher numberMatcher = numberPattern.matcher(rowString);
+            while(numberMatcher.find()) {
+                addToGearMap(numberMatcher.group(), numberMatcher.start(), i);
+            }
+        }
+    }
+
+    private static void addToGearMap(String candidate, Integer col, Integer row) {
+        for (int i = row - 1; i <= row + 1; i++) {
+            for (int j = col - 1; j <= col + candidate.length(); j++) {
+                if (i >= 0 && i < MAX_LENGTH && j >= 0 && j < MAX_LENGTH) {
+                    if (getCharAt(j, i).equals('*')) {
+                        gearCandidates.get(i).get(j).add(Integer.parseInt(candidate));
+                    }
+                }
+            }
+        }
     }
     
     private static Character getCharAt(Integer col, Integer row) {
