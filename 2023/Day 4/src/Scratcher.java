4ade4b3fa567121d51462320a77bd25383778493
roleyguacamole <tyler.barry@uconn.edu>
2024-01-08 16:17:12 -0500

    Add solution in Java for Y2023 D04 Part 2

--- 2023/Day 4/src/Scratcher.java
+++ 2023/Day 4/src/Scratcher.java
@@ -1,27 +1,43 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Scratcher {
+    private static final HashMap<Integer, Integer> tickets = new HashMap<>();
     private static final Pattern numberPattern = Pattern.compile("\\d+");
     public static void main(String[] args) throws FileNotFoundException {
         final File input = new File("../data/input.txt");
         final Scanner inputReader = new Scanner(input);
+        final Scanner inputReader2 = new Scanner(input);
 
+        // Part 1 and initialize the map for Part 2
         Integer totalPoints = 0;
+        Integer ticketNumber = 1;
         while (inputReader.hasNextLine()) {
+            tickets.put(ticketNumber, 1);
+            ticketNumber++;
             totalPoints += getScore(inputReader.nextLine());
         }
-
         inputReader.close();
 
+        // Part 2
+        Integer totalTickets = 0;
+        ticketNumber = 1;
+        while (inputReader2.hasNextLine()) {
+            totalTickets += getTickets(inputReader2.nextLine(), tickets.get(ticketNumber), ticketNumber);
+            ticketNumber++;
+        }
+        inputReader2.close();
+
         System.out.println(String.format("Answer 1: %d", totalPoints));
+        System.out.println(String.format("Answer 2: %d", totalTickets));
     }
 
-    private static Integer getScore(String scratchCard) {
+    private static Integer getNumberOfWinners(String scratchCard) {
         // Get the winning numbers and our numbers from the card
         final HashSet<Integer> winningNumbers = new HashSet<Integer>();
         final HashSet<Integer> ourNumbers = new HashSet<Integer>();
@@ -34,20 +50,34 @@ public class Scratcher {
             else if (currentNumber > 10) {
                 ourNumbers.add(Integer.parseInt(numberMatcher.group()));
             }
-            currentNumber += 1;
+            currentNumber++;
         }
 
         // Get the intersection of winning numbers and our numbers
         winningNumbers.retainAll(ourNumbers);
 
-        // Get the score of the scratcher based on the size of the intersection
+        return winningNumbers.size();
+    }
+
+    private static Integer getScore(String scratchCard) {
+        final Integer numWinners = getNumberOfWinners(scratchCard);
         final Integer score;
-        if (winningNumbers.size() == 0) {
+        if (numWinners == 0) {
             score = 0;
         }
         else {
-            score = Double.valueOf(Math.pow(2, winningNumbers.size() - 1)).intValue();
+            score = Double.valueOf(Math.pow(2, numWinners - 1)).intValue();
         }
         return score;
+    }
+
+    private static Integer getTickets(String scratchCard, Integer numberOfTickets, Integer currentTicket) {
+        final Integer numWinners = getNumberOfWinners(scratchCard);
+        for (int i = currentTicket + 1; i <= currentTicket + numWinners; i++) {
+            if (tickets.keySet().contains(i)) {
+                tickets.put(i, tickets.get(i) + numberOfTickets);
+            }
+        }
+        return numberOfTickets;
     }
 }
